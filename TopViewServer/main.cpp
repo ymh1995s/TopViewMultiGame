0d6e3021e2441c54a76451fb703c84399d07b4f2
DESKTOP-SLNADE2\user <qq11123@naver.com>
2025-10-29 23:24:49 +0900

    ASIO 사용환경 구축 + pch 설정
    ASIO 라이브러리 파일은 커서 ignore

---
+++ TopViewServer/main.cpp
@@ -0,0 +1,10 @@
+#include <SDKDDKVer.h>
+#include "pch.h"
+
+
+int main()
+{
+	boost::asio::io_context io_context; // IOCP를 추상화한 ASIO 객체
+
+	return 0;
+}
---
+++ TopViewServer/pch.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <queue>
+
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
